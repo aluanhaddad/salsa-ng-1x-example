0b2f3d014e62a8167310c0ffe99d6016e10308d4
Aluan Haddad <aluanh@gmail.com>
2016-07-11 01:04:31 -0400

    Initial commit

---
+++ app.js
@@ -0,0 +1,24 @@
+(function () {
+    'use strict';
+
+    angular
+        .module('app')
+        .controller('TestController', TestController);
+
+    TestController.$inject = ['$q', '$http', '$timeout'];
+    /**
+     * @param {ng.IQService} $q
+     * @param {ng.IHttpService} $http
+     * @param {ng.ITimeoutService} $timeout
+     */
+    function TestController($q, $http, $timeout) {
+        $timeout(()=> $q.resolve('test')).
+        var vm = this;
+
+        activate();
+
+        function activate() {
+
+        }
+    }
+})();
